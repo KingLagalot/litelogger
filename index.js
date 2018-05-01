ee424e077cf68c18f317e8ac26d507a81de21ec2
Nathaniel Heitsch <njh9417@rit.edu>
2018-05-01 14:59:17 -0400

    Added timestamp ability

---
+++ index.js
@@ -0,0 +1,48 @@
+    exports.init = function(name, timestamp){
+        this.name = name;
+        this.timestamp = timestamp;
+        return this;
+    };
+    exports.make = function(output, message, flag, color){
+        var flags = '['+this.name+']';
+        if(this.timestamp){
+            flags = '['+new Date().toLocaleTimeString('en-US', { hour12: false })+']'+flags;
+        }
+        if(flag != undefined){
+            flags += '['+flag+']';
+        }
+        output = '\x1b['+color+'m' + output + flags + '\x1b[0m';
+        if(flags == ""){
+            output += message;
+        }else{
+            output += ' ' + message;
+        }
+        return output;
+    };
+
+    exports.debug = function(message, flag){
+        var output = '   debug - ';
+        message = '\x1b[2m' + message + '\x1b[0m';
+        output = this.make(output,message,flag,'34');
+        console.log(output);
+    };
+
+    exports.info = function(message, flag){
+        var output = '   info  - ';
+        output = this.make(output,message,flag,'36');
+        console.log(output);
+    };
+
+    exports.warning = function(message, flag){
+        var output = ' warning - ';
+        output = this.make(output,message,flag,'93');
+        console.log(output);
+
+    };
+
+    exports.error = function(message, flag){
+        var output = '   error - ';
+        output = this.make(output,message,flag,'31');
+        console.log(output);
+
+    };
---
+++ test.js
@@ -0,0 +1,11 @@
+var llog = require('./index').init('testclass');
+
+llog.warning('This is a warning');
+llog.debug('This is a debug message!');
+llog.info('Just wanted to let you know this info.');
+llog.error('Something really bad just happened :(');
+
+llog.info('You can also add a flag', 'I\'m the flag');
+
+var llogtime = require('./index').init('testclass', true);
+llogtime.info('You can also add a time stamp', 'Example with time stamp');
